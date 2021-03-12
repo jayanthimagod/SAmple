b0b4c0def7a6b16d08e4c1e6b80cc22d6e7f30ef
91948 <91948@DESKTOP-KR0BRDU>
2021-03-12 17:42:16 +0530

    sample

---
+++ src/test/java/com/victorSolutions/Sample.java
@@ -0,0 +1,15 @@
+package com.victorSolutions;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	@Test
+	public void createOrg(){
+		System.out.println("Organization should be created");
+	}
+	@Test
+	public void createProduct(){
+		System.out.println("Product should be created");
+}
+}
---
+++ src/test/java/com/victorSolutions/SampleClass.java
@@ -0,0 +1,5 @@
+package com.victorSolutions;
+
+public class SampleClass {
+
+}
